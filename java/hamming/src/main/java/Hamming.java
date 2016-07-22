2c40e962edc7276a28df2bf809f4ef82023fe302
Arun <arun@in-arunsathin.local>
2016-07-22 13:54:25 +0530

    aviod spliting by using charAt

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -3,11 +3,9 @@ public class Hamming {
         if (trand1.length() != trand2.length()) {
             throw new IllegalArgumentException();
         }
-        String[] splitedtrand2 = trand2.split("");
-        String[] splitedTrand1 = trand1.split("");
         int counter = 0;
         for (int i = 0; i < trand1.length(); i++) {
-            if (!(splitedTrand1[i].equals(splitedtrand2[i]))) {
+            if (trand1.charAt(i) != trand2.charAt(i)) {
                 counter++;
             }
         }
